da7fe68365d9272f87a140b06813f20244546857
tainguyennls <nhuutai2698@gmail.com>
2019-01-16 12:44:29 +0700

    update rsa

--- src/com/nht/security/ui/RSAView.java
+++ src/com/nht/security/ui/RSAView.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.awt.Font;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Arrays;
+import java.util.Base64;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
@@ -31,106 +33,63 @@ public class RSAView extends JPanel {
 	private RSAInitKey rsaInitKey;
 	private File fSrcPrivateKey;
 	private File fSrcPublicKey;
+	private JButton btnEncrypt;
 	private JButton btnDecrypt;
 	private JButton btnGenerate;
 	private JButton btnBrowseDecrypt;
+	private JButton btnBrowseEncrypt;
 	private JTextField txtPathPrivateKey;
+	private JTextField txtPathPublicKey;
 	private JTextField txtInputDecrypt;
+	private JTextField txtInputEncrypt;
 	private JTextArea textAreaOutput;
 	private JComboBox<?> comboBoxKeySize;
 	private JComboBox<?> comboBoxOptions;
 	private String [] options = {"ENCRYPT","DECRYPT"};
 	private String [] arrayBitsKey = { "512", "1024", "2048", "4096" };
 	private JButton btnExport;
-	private String selectedOption;
+	private String selectedOption ="";
 	private JTextField textField;
+	private JPanel panelDecrypt;
+	private JPanel panelEncrypt;
+	private JPanel jplEncryptDecrypt;
 
 	public RSAView() {
 
 		setLayout(null);
 		rsaInitKey = new RSAInitKey();
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setLayout(null);
-		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "DECRYPT & DECRYPT", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel_1.setBounds(25, 107, 590, 112);
-		add(panel_1);
+		jplEncryptDecrypt = new JPanel();
+		jplEncryptDecrypt.setLayout(null);
+		jplEncryptDecrypt.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "DECRYPT & DECRYPT", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		jplEncryptDecrypt.setBounds(25, 107, 590, 112);
+		add(jplEncryptDecrypt);
 
-		JLabel lblPrivateKey = new JLabel("Private key");
-		lblPrivateKey.setBounds(195, 23, 61, 14);
-		panel_1.add(lblPrivateKey);
-
-		txtPathPrivateKey = new JTextField();
-		txtPathPrivateKey.setColumns(10);
-		txtPathPrivateKey.setBounds(255, 20, 238, 20);
-		panel_1.add(txtPathPrivateKey);
-
-		btnBrowseDecrypt = new JButton("");
-		btnBrowseDecrypt.addActionListener(e -> {
-
-			JFileChooser jFileChooserPrivateKey = new JFileChooser();
-			int stasus = jFileChooserPrivateKey.showOpenDialog(null);
-			if (stasus == JFileChooser.APPROVE_OPTION) {
-				fSrcPrivateKey = jFileChooserPrivateKey.getSelectedFile();
-				try {
-					privateKey = new RSAReadKey().readPrivateKey("private_key.k");
-					txtPathPrivateKey.setText(fSrcPrivateKey.getPath());
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-			}
-
-		});
-		btnBrowseDecrypt.setText("Browse");
-		btnBrowseDecrypt.setBounds(503, 19, 77, 23);
-		panel_1.add(btnBrowseDecrypt);
-
-		txtInputDecrypt = new JTextField();
-		txtInputDecrypt.setColumns(10);
-		txtInputDecrypt.setBounds(255, 49, 325, 20);
-		panel_1.add(txtInputDecrypt);
-
-		JLabel label_1 = new JLabel("Input");
-		label_1.setBounds(195, 52, 50, 14);
-		panel_1.add(label_1);
-
-		btnDecrypt = new JButton("Decrypt");
-		btnDecrypt.addActionListener(e -> {
-
-			String txtDe = txtInputDecrypt.getText().trim();
-			
-			if (!"".equals(txtDe) && null != fSrcPrivateKey) 
-			{
-				if (fSrcPrivateKey.getName().endsWith(".k")) 
-				{
-					byte[] input = txtDe.getBytes();
-					try {
-						byte[] outDecrypt = new RSA().decrypt(input, privateKey);
-						textAreaOutput.setText(new String(outDecrypt));
-					} catch (Exception e1) {
-						e1.printStackTrace();
-					}
-				}
-				else 
-				{
-					JOptionPane.showMessageDialog(getParent(), "Your extension of key must be \".k\" ");
-				}
-			}
-		});
-		btnDecrypt.setBounds(502, 78, 78, 23);
-		panel_1.add(btnDecrypt);
+		createEncryptLayout();
+		createDecryptLayout();
 		
 		JLabel lblOptions = new JLabel("Options");
 		lblOptions.setBounds(10, 52, 43, 14);
-		panel_1.add(lblOptions);
+		jplEncryptDecrypt.add(lblOptions);
 
 		comboBoxOptions = new JComboBox<Object>(options);
 		comboBoxOptions.addActionListener(e -> {
 			selectedOption = (String) comboBoxOptions.getSelectedItem();
+
+			if(selectedOption.equals("DECRYPT")){
+				panelEncrypt.setVisible(false);
+				panelDecrypt.setVisible(true);
+			}
+			else if(selectedOption.equals("ENCRYPT")){
+				panelEncrypt.setVisible(true);
+				panelDecrypt.setVisible(false);
+			}
+			repaint();
 		});
 		comboBoxOptions.setSelectedIndex(0);
+		comboBoxOptions.setFocusable(false);
 		comboBoxOptions.setBounds(63, 49, 89, 20);
-		panel_1.add(comboBoxOptions);
+		jplEncryptDecrypt.add(comboBoxOptions);
 
 		JLabel lblOutput = new JLabel("OUTPUT");
 		lblOutput.setBounds(25, 294, 46, 14);
@@ -144,15 +103,15 @@ public class RSAView extends JPanel {
 		textAreaOutput.setBounds(25, 319, 590, 70);
 		add(textAreaOutput);
 
-		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(new TitledBorder(null, "Generate and Export Key", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_2.setBounds(25, 31, 590, 61);
-		add(panel_2);
-		panel_2.setLayout(null);
+		JPanel jplGeExport = new JPanel();
+		jplGeExport.setBorder(new TitledBorder(null, "Generate and Export Key", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		jplGeExport.setBounds(25, 31, 590, 61);
+		add(jplGeExport);
+		jplGeExport.setLayout(null);
 
 		JLabel lblKeySize = new JLabel("Key size");
 		lblKeySize.setBounds(10, 28, 46, 14);
-		panel_2.add(lblKeySize);
+		jplGeExport.add(lblKeySize);
 
 		comboBoxKeySize = new JComboBox<Object>(arrayBitsKey);
 		comboBoxKeySize.addActionListener(e -> {
@@ -160,26 +119,175 @@ public class RSAView extends JPanel {
 			keySize = Integer.parseInt(stringBits);
 		});
 		comboBoxKeySize.setSelectedIndex(0);
+		comboBoxKeySize.setFocusable(false);
 		comboBoxKeySize.setBounds(61, 25, 89, 20);
-		panel_2.add(comboBoxKeySize);
+		jplGeExport.add(comboBoxKeySize);
 
 		btnGenerate = new JButton("Generate");
 		btnGenerate.addActionListener(e -> {
 			rsaInitKey = new RSAInitKey();
 			rsaInitKey.initKey(keySize);
-			JOptionPane.showMessageDialog(getParent(), "Generated key successfull");
+			JOptionPane.showMessageDialog(getParent(), "Generated key successful");
 		});
 		btnGenerate.setBounds(160, 24, 89, 23);
-		panel_2.add(btnGenerate);
+		jplGeExport.add(btnGenerate);
 		
 		btnExport = new JButton("Export");
 		btnExport.setBounds(259, 24, 89, 23);
-		panel_2.add(btnExport);
+		jplGeExport.add(btnExport);
 
 		JScrollPane scrollBar = new JScrollPane(textAreaOutput);
 		scrollBar.setAutoscrolls(true);
 		scrollBar.setBounds(25, 319, 590, 70);
 		add(scrollBar);
 
+	}
+
+	public void createEncryptLayout(){
+
+		panelEncrypt = new JPanel();
+		panelEncrypt.setBounds(195, 19, 385, 82);
+		jplEncryptDecrypt.add(panelEncrypt);
+		panelEncrypt.setLayout(null);
+
+		JLabel lblPublicKey = new JLabel("Public key");
+		lblPublicKey.setBounds(0, 4, 61, 14);
+		panelEncrypt.add(lblPublicKey);
+
+		txtPathPublicKey = new JTextField();
+		txtPathPublicKey.setBounds(60, 1, 238, 20);
+		panelEncrypt.add(txtPathPublicKey);
+		txtPathPublicKey.setColumns(10);
+
+		btnBrowseEncrypt = new JButton("");
+		btnBrowseEncrypt.setBounds(308, 0, 77, 23);
+		panelEncrypt.add(btnBrowseEncrypt);
+		btnBrowseEncrypt.addActionListener(e -> {
+
+			JFileChooser jFileChooserPublicKey = new JFileChooser();
+			int s = jFileChooserPublicKey.showOpenDialog(null);
+			if (s == JFileChooser.APPROVE_OPTION) {
+				fSrcPublicKey = jFileChooserPublicKey.getSelectedFile();
+				try {
+					publicKey = new RSAReadKey().readPublicKey("public_key.k");
+					txtPathPublicKey.setText(fSrcPublicKey.getPath());
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+
+		});
+
+		btnBrowseEncrypt.setText("Browse");
+
+		txtInputEncrypt = new JTextField();
+		txtInputEncrypt.setBounds(60, 30, 325, 20);
+		panelEncrypt.add(txtInputEncrypt);
+		txtInputEncrypt.setColumns(10);
+
+		JLabel lbl = new JLabel("Input");
+		lbl.setBounds(0, 33, 50, 14);
+		panelEncrypt.add(lbl);
+
+		btnEncrypt = new JButton("Encrypt");
+		btnEncrypt.setBounds(307, 59, 78, 23);
+		panelEncrypt.add(btnEncrypt);
+		btnEncrypt.addActionListener(e -> {
+
+			String txtDe = txtInputEncrypt.getText().trim();
+
+			if (!"".equals(txtDe) && null != fSrcPublicKey)
+			{
+				if (fSrcPublicKey.getName().endsWith(".k"))
+				{
+					byte[] input = txtDe.getBytes();
+					try {
+						byte[] outEncrypt = new RSA().encrypt(input, publicKey);
+						textAreaOutput.setText(Base64.getEncoder().encodeToString(outEncrypt));
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(getParent(), "Your extension of key must be \".k\" ");
+				}
+			}
+		});
+	}
+
+	public void createDecryptLayout(){
+
+		panelDecrypt = new JPanel();
+		panelDecrypt.setBounds(195, 19, 385, 82);
+		jplEncryptDecrypt.add(panelDecrypt);
+		panelDecrypt.setLayout(null);
+
+
+		JLabel lblPrivateKey = new JLabel("Private key");
+		lblPrivateKey.setBounds(0, 4, 61, 14);
+		panelDecrypt.add(lblPrivateKey);
+
+		txtPathPrivateKey = new JTextField();
+		txtPathPrivateKey.setBounds(60, 1, 238, 20);
+		panelDecrypt.add(txtPathPrivateKey);
+		txtPathPrivateKey.setColumns(10);
+
+		btnBrowseDecrypt = new JButton("Browse");
+		btnBrowseDecrypt.setFocusable(false);
+		btnBrowseDecrypt.setBounds(308, 0, 77, 23);
+		panelDecrypt.add(btnBrowseDecrypt);
+		btnBrowseDecrypt.addActionListener(e -> {
+
+			JFileChooser jFileChooserPrivateKey = new JFileChooser();
+			int st = jFileChooserPrivateKey.showOpenDialog(null);
+			if (st == JFileChooser.APPROVE_OPTION) {
+				fSrcPrivateKey = jFileChooserPrivateKey.getSelectedFile();
+				try {
+					privateKey = new RSAReadKey().readPrivateKey("private_key.k");
+					txtPathPrivateKey.setText(fSrcPrivateKey.getPath());
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+
+		});
+
+		txtInputDecrypt = new JTextField();
+		txtInputDecrypt.setBounds(60, 30, 325, 20);
+		panelDecrypt.add(txtInputDecrypt);
+		txtInputDecrypt.setColumns(10);
+
+		JLabel lblInput = new JLabel("Input");
+		lblInput.setBounds(0, 33, 50, 14);
+		panelDecrypt.add(lblInput);
+
+		btnDecrypt = new JButton("Decrypt");
+		btnEncrypt.setFocusable(false);
+		btnDecrypt.setBounds(307, 59, 78, 23);
+		panelDecrypt.add(btnDecrypt);
+		btnDecrypt.addActionListener(e -> {
+
+			String tmp = txtInputDecrypt.getText().trim();
+
+			if (!"".equals(tmp) && null != fSrcPrivateKey)
+			{
+				if (fSrcPrivateKey.getName().endsWith(".k"))
+				{
+					byte[] input = tmp.getBytes();
+					try {
+						byte[] outDecrypt = new RSA().decrypt(input,privateKey);
+						textAreaOutput.setText(new String(outDecrypt));
+						System.out.println(new String(outDecrypt));
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(getParent(), "Your extension of key must be \".k\" ");
+				}
+			}
+		});
 	}
 }
